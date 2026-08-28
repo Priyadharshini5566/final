f96cdafd84921383ea246138df005e464e349313
Priyadharshini5566 <Priyadharshini248priya@gmail.com>
2026-08-28 14:03:25 +0530

    Fix CORS

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: process.env.CLIENT_URL?.split(",").map(v => v.trim()) || ["http://localhost:5173"],
+  origin: "https://final-2-ke5o.onrender.com",
   credentials: true,
 }));
 app.use(express.json({ limit: "1mb" }));
